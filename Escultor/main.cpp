04ad97bf0ece8c8634de73eb30d85a50e7a48710
MarcosBandeira <mvbirigoyen@gmail.com>
2023-05-20 11:23:30 -0300

    Add CMakeLists.txt

--- Escultor/main.cpp
+++ Escultor/main.cpp
@@ -3,13 +3,22 @@
 
 int main(){
     
-    Sculptor teste = Sculptor(10, 10, 10);
+    Sculptor teste = Sculptor(50, 50, 50);
+    
     teste.setColor(1.0,0.0,0.0,1.0);
     
-    teste.putEllipsoid(5,5,5, 1, 2, 1);
+    teste.putBox(49,40,49,45,49,40);
+
+    teste.setColor(0.0,0.0,1.0,1.0);
+
+    teste.putEllipsoid(10,5,10, 10, 5, 8);
+
+    teste.setColor(0.0,1.0,0.0,1.0);
+
+    teste.putSphere(40,20,20, 7);
     
     //teste.putSphere(5,5,5,1);
 
-    teste.writeOFF("marreco.off");
+    teste.writeOFF("exemplo.off");
     return 0;
 }
